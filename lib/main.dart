d2e058ac718c9331b6b029ca7c0977ec017a9237
fermfirxzer <jirayusfirxzer@gmail.com>
2024-12-17 17:08:50 +0700

    add route to charging page

--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,9 @@
 import 'package:flutter/material.dart';
+import './page/changing_page.dart';
 
 void main() {
-  runApp(const Change());  // This is where you start the app
+  runApp(const Myapp()); // This is where you start the app
 }
-
-class Change extends StatelessWidget {
-  const Change({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        appBarTheme: const AppBarTheme(color: Colors.red),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-      ),
-      home: const ChangingPage(),  // Set ChangingPage as the home widget
-    );
-  }
-}
-
-class ChangingPage extends StatefulWidget {
-  const ChangingPage({super.key});
-
-  @override
-  State<ChangingPage> createState() => _ChangingPageState();
-}
-
-class _ChangingPageState extends State<ChangingPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Changing Page'),
-      ),
-      body: const Center(
-        child: 
-        Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Current SOC %",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter your Current SOC %',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                    ),
-                ],
-              ),
-              SizedBox(height: 20,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Target SOC %  ",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter your Current SOC %',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                    ),
-                ],
-              ),
-             
-               SizedBox(height: 20,),
-              Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      "Change rate A ",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                  // Expanded to avoid overflow when the textfield expands
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter your Change rate',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                    ),
-                ],
-              ),
-              SizedBox(height: 20,),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Testdadadass",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  Expanded(child: TextField(decoration: InputDecoration(labelText: "Enter your change",border: OutlineInputBorder()),))
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 
 class Myapp extends StatelessWidget {
   const Myapp({super.key});
@@ -130,146 +16,230 @@ class Myapp extends StatelessWidget {
           appBarTheme: const AppBarTheme(color: Colors.red),
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Center(
-                child: Text("My appbar",
-                    style: TextStyle(fontSize: 24, color: Colors.white))),
-            leading: IconButton(
-                onPressed: () {
-                  debugPrint("This is title!");
-                },
-                icon: const Icon(Icons.menu)),
-            actions: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.home, color: Colors.white),
-              ),
-            ],
+        home: const Homepage());
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text("My appbar",
+                style: TextStyle(fontSize: 24, color: Colors.white))),
+        leading: IconButton(
+            onPressed: () {
+              debugPrint("This is title!");
+            },
+            icon: const Icon(Icons.menu)),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.home, color: Colors.white),
           ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Column(
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                            20.0), // Adjust the radius as needed
+                        child: Image.asset(
+                          'assets/images/dash-fainting.gif',
+                          width: 250,
+                          height: 200,
+                          fit: BoxFit
+                              .cover, // Ensures the image is properly fitted within the specified dimensions
+                        ),
+                      ),
+                      const Text(
+                        "ขอบคุณที่ใช้บริการ App ของเรา",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const Text(
+                        "เรายินดีที่ได้เป็นส่วนหนึ่งในการเดินทางของคุณ",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.normal),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text("สรุปรายละเอียดการชาร์จ",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(
-                                20.0), // Adjust the radius as needed
-                            child: Image.asset(
-                              'assets/images/dash-fainting.gif',
-                              width: 250,
-                              height: 200,
-                              fit: BoxFit
-                                  .cover, // Ensures the image is properly fitted within the specified dimensions
-                            ),
-                          ),
-                          const Text(
-                            "ขอบคุณที่ใช้บริการ App ของเรา",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                          const Text(
-                            "เรายินดีที่ได้เป็นส่วนหนึ่งในการเดินทางของคุณ",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.normal),
-                          ),
+                          Icon(Icons.calendar_month),
+                          Expanded(
+                              child: Text(
+                            "วันที่ชาร์จ",
+                            overflow: TextOverflow.ellipsis,
+                          )),
+                          Text("9 กันยายน 2565"),
                         ],
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Column(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text("สรุปรายละเอียดการชาร์จ",
-                              style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold)),
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
                             children: [
-                              Icon(Icons.calendar_month),
-                              Expanded(
+                              Icon(Icons.baby_changing_station),
+                              Text("สถานีชาร์จ"),
+                            ],
+                          ),
+                          Text("100 บาท"),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.link),
+                              Text("ประเภทการชาร์จ"),
+                            ],
+                          ),
+                          Text("CCS2"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    children: [
+                      const Center(
+                        child: Text("ประวัติการชาร์จ"),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Table(
+                        border: TableBorder.all(color: Colors.black),
+                        defaultVerticalAlignment:
+                            TableCellVerticalAlignment.middle,
+                        children: const [
+                          TableRow(
+                              decoration: BoxDecoration(color: Colors.white),
+                              children: [
+                                TableCell(
+                                    verticalAlignment:
+                                        TableCellVerticalAlignment.middle,
+                                    child: Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text("วันที่ชาร์จ"),
+                                    )),
+                                TableCell(
+                                    child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text("ประเภทการชาร์จ"),
+                                )),
+                                TableCell(
+                                    child: Padding(
+                                  padding: EdgeInsets.all(8.0),
                                   child: Text(
-                                "วันที่ชาร์จ",
-                                overflow: TextOverflow.ellipsis,
-                              )),
-                              Text("9 กันยายน 2565"),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Icon(Icons.baby_changing_station),
-                                  Text("สถานีชาร์จ"),
-                                ],
-                              ),
-                              Text("100 บาท"),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Icon(Icons.link),
-                                  Text("ประเภทการชาร์จ"),
-                                ],
-                              ),
-                              Text("CCS2"),
-                            ],
-                          ),
+                                    "รายละเอียดการชาร์จ",
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ))
+                              ]),
+                          TableRow(
+                              decoration: BoxDecoration(color: Colors.white),
+                              children: [
+                                TableCell(
+                                    verticalAlignment:
+                                        TableCellVerticalAlignment.middle,
+                                    child: Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text("9 กันยายน"),
+                                    )),
+                                TableCell(
+                                    child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text("100 บาท"),
+                                )),
+                                TableCell(
+                                    child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text("CCS2"),
+                                ))
+                              ])
                         ],
                       ),
-                    ),
-                    Center(
-                      child: Card(
-                        elevation: 10,
-                        margin: const EdgeInsets.all(10),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          side: const BorderSide(
-                            color: Colors.red, // Border color
-                            width: 2.0, // Border width
-                          ),
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.all(20),
-                          child: Text(
-                            "ราคา :100 บาท",
-                          ),
-                        ),
+                    ],
+                  ),
+                ),
+                Center(
+                  child: Card(
+                    elevation: 10,
+                    margin: const EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      side: const BorderSide(
+                        color: Colors.red, // Border color
+                        width: 2.0, // Border width
                       ),
                     ),
-                    const SizedBox(
-                      height: 10,
+                    child: const Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Text(
+                        "ราคา :100 บาท",
+                      ),
                     ),
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.all(10),
-                      color: Colors.red,
-                      child: const Center(
-                          child: Text("ค่าบริการรวมทั้งสิน : 100 บาท")),
-                    )
-                  ]),
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                debugPrint("TAB pressed");
-              },
-              child: const Icon(Icons.exit_to_app)),
-        ));
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  margin: const EdgeInsets.all(10),
+                  color: Colors.red,
+                  child: const Center(
+                      child: Text("ค่าบริการรวมทั้งสิน : 100 บาท")),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Change(),
+                        ),
+                      );
+                    },
+                    child: const Text("Press to go to Changing page"))
+              ]),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("TAB pressed");
+          },
+          child: const Icon(Icons.exit_to_app)),
+    );
   }
 }
--- lib/page/changing_page.dart
+++ lib/page/changing_page.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/main.dart';
+
+class Change extends StatelessWidget {
+  const Change({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Change Page'),
+        ),
+        body: const ChangingPage());
+  }
+}
+
 class ChangingPage extends StatefulWidget {
   const ChangingPage({super.key});
 
@@ -10,16 +23,151 @@ class ChangingPage extends StatefulWidget {
 class _ChangingPageState extends State<ChangingPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [ElevatedButton(
-          onPressed: () {
-            // Navigate to the ChangingPage
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const Myapp()),
-            );
-          },
-          child: const Text('Go to Changing Page'),
-        ),],
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          const Text("กรอกรายละเอียดการชาร์จที่ต้องการ"),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("Current SOC %"),
+                  SizedBox(
+                    width: 100,
+                    child: TextField(
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                    ),
+                  ),
+                ],
+              ),
+              Image.network(
+                "https://cdn2.iconfinder.com/data/icons/car-14/100/Car_Paint-512.png",
+                width: 100,
+                height: 75,
+              ),
+              const Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("Target SOC %"),
+                  SizedBox(
+                    width: 100,
+                    child: TextField(
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.charging_station_rounded),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text("Charge rate A")
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 200,
+                      height: 40,
+                      child: TextField(
+                        obscureText: false,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Charge rate"),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.electric_meter),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text("Volt")
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 200,
+                      height: 40,
+                      child: TextField(
+                        obscureText: false,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(), labelText: "Volt"),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.power),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text("W charge")
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 200,
+                      height: 40,
+                      child: TextField(
+                        obscureText: false,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "W charge"),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("เวลาที่ใช้ชาร์จโดยเฉลี่ย : 1000.000 hrs")
+              ],
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
